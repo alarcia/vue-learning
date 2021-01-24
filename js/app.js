bfe539f02defa02c0d3dc444b8ff1d6710f68c4b
alarcia <jabogood@gmail.com>
2021-01-24 19:55:54 +0100

    computed and methods

--- js/app.js
+++ js/app.js
@@ -3,5 +3,18 @@ var app = new Vue({
   data: {
     message: 'Hello Vue',
     sum: 3+6,
+    value: 5
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    calculateSum(value1, value2){
+      return value1 + value2;
+    }
   }
 })
